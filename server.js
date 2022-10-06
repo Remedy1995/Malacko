d77e169ad82a7213692843622845d93af021fe6a
Remedy1995 <adjetadjetey45@gmail.com>
2022-10-06 11:12:43 +0000

    hi

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 // app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(express.static(path.join(__dirname + '/dist/malacko')));
+app.use(express.static(path.join(__dirname + '/public/dist/malacko')));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/malacko/index.html'));});
+'/public/dist/malacko/index.html'));});
 app.use('/shipping',shipping);
 app.use('/mapping',mapping)
 app.use(bodyparser.urlencoded({extended:true}));
