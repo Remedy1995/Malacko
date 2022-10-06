ef3d29c77485fcad52cd518c3350c1f08f48fb31
Remedy1995 <adjetadjetey45@gmail.com>
2022-10-06 11:07:05 +0000

    hi

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 // app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); 
-app.use(express.static(__dirname + '/dist/malacko'));
+app.use(express.static(path.join(__dirname + '/dist/malacko')));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/malacko/index.html'));});
