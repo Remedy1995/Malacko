c749d2ddd539f5eeef785f96c1a089482a1c42f4
Remedy1995 <adjetadjetey45@gmail.com>
2022-10-06 09:24:08 +0000

    css

--- server.js
+++ server.js
@@ -10,16 +10,14 @@ const shipping=require('./routes/Shipping');
 const connection=require('./controller/Auth');
 connection();//database;
 app.use(cors());
-app.use('/assets', express.static(path.join(__dirname, '/dist/malacko/public')))
-app.use("/assets", express.static(__dirname + "/assets"));
+// app.use('/assets', express.static(path.join(__dirname, '/dist/malacko/public')))
+// app.use("/assets", express.static(__dirname + "/assets"));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(express.static(__dirname + '/dist/malacko'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/malacko/index.html'));});
-// app.use(express.static(path.join(__dirname, 'dist','malacko')));
-// res.sendFile(path.join(__dirname,'dist','malacko','index.html'));
 app.use('/shipping',shipping);
 app.use('/mapping',mapping)
 app.use(bodyparser.urlencoded({extended:true}));
