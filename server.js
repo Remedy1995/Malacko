68babd317f11df8f3d7bae8918d9963d69b9e7be
Remedy1995 <adjetadjetey45@gmail.com>
2022-10-06 03:13:27 +0000

    css

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const shipping=require('./routes/Shipping');
 const connection=require('./controller/Auth');
 connection();//database;
 app.use(cors());
+app.use(express.static(__dirname + '../public'));
 app.use(express.json());
-app.use(express.static(__dirname + '/dist/malacko/public'));
+
 app.use(express.static(__dirname + '/dist/malacko'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
